3137f64f952ee7e0743e7d7cb26cf264d7e82583
Ransome Madziyauswa <rmadziyauswa@gmail.com>
2025-01-08 16:29:24 +0200

    Removed obsolete libraries, updated tufte template imports, init Venn.js

--- glasseye/js/modular/Venn.js
+++ glasseye/js/modular/Venn.js
@@ -1,52 +1,44 @@
-var Venn = function(processed_data, div, size) {
-
+var Venn = function (processed_data, div, size) {
   margin = {
     top: 5,
     bottom: 5,
     left: 5,
-    right: 5
+    right: 5,
   };
 
   GlasseyeChart.call(this, div, size, margin);
 
   this.processed_data = processed_data;
 
-  this.venn_chart = venn.VennDiagram()
-    .width(this.width)
-    .height(this.height);
-
-
-
+  this.venn_chart = venn.VennDiagram().width(500).height(400);
 };
 
 Venn.prototype = Object.create(GlasseyeChart.prototype);
 
-Venn.prototype.add_venn = function() {
-
+Venn.prototype.add_venn = function () {
   this.chart_area.datum(this.processed_data).call(this.venn_chart);
+  this.chart_area.selectAll(".venn-circle path").style("fill-opacity", 0.5);
 
+  this.chart_area
+    .selectAll(".venn-circle text")
+    .style("font-size", "14px")
+    .style("fill", "#000");
 };
 
-
 function venn(data, div, size) {
-
-  var inline_parser = function(data) {
+  var inline_parser = function (data) {
     return data;
   };
 
-  var csv_parser = function(data) {
+  var csv_parser = function (data) {
     return data;
   };
 
-  var draw = function(processed_data, div, size) {
-
-
+  var draw = function (processed_data, div, size) {
     var glasseye_chart = new Venn(processed_data, div, size);
 
     glasseye_chart.add_svg().add_venn();
-
   };
 
   build_chart(data, div, size, undefined, csv_parser, inline_parser, draw);
-
 }
