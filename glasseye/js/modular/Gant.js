995965412199d919a536423eb12069a70640b8c5
Ransome Madziyauswa <rmadziyauswa@gmail.com>
2025-01-13 15:58:09 +0200

    gantt charts init implementation

--- glasseye/js/modular/Gant.js
+++ glasseye/js/modular/Gant.js
@@ -1,109 +1,76 @@
-var Gantt = function(processed_data, div, size, scales) {
-
+var Gantt = function (processed_data, div, size, scales) {
   this.div = div;
   this.processed_data = processed_data;
 
-  GridChart.call(this, div, size, ["Time", "Tasks"], scales, {
-    top: 20,
-    bottom: 80,
-    left: 80,
-    right: 20
-  });
+  // Scales
+  this.xScale = d3
+    .scaleTime()
+    .domain([d3.min(this.processed_data, (d) => d.start), d3.max(this.processed_data, (d) => d.end)])
+    .range([0, width]);
 
-  this.tip = d3.tip()
-    .attr('class', 'd3-tip')
-    .offset([-10, 0])
-    .html(function(d) {
-      return Math.floor((d.end - d.start) / (1000 * 60 * 60 * 24)) + " days";
-    });
+  this.yScale = d3
+    .scaleBand()
+    .domain(this.processed_data.map((d) => d.task))
+    .range([0, this.height])
+    .padding(0.2);
 
-  this.bar_width = this.width / processed_data.length;
+  // Axes
+  this.xAxis = d3
+    .axisBottom(xScale)
+    .ticks(d3.timeWeek.every(1))
+    .tickFormat(d3.timeFormat("%d/%m/%Y"));
+  this.yAxis = d3.axisLeft(yScale);
 
 };
 
 Gantt.prototype = Object.create(GridChart.prototype);
 
-Gantt.prototype.add_tasks = function() {
+Gantt.prototype.add_tasks = function () {
 
-  var x_scale = this.x,
-    y_scale = this.y,
-    bar_width = this.bar_width;
+  this.chart_area
+    .append("g")
+    .attr("class", "x-axis")
+    .attr("transform", `translate(0,${this.height})`)
+    .call(this.xAxis)
+    .selectAll("text")
+    .attr("transform", "rotate(-45)")
+    .style("text-anchor", "end");
 
-  this.chart_area.call(this.tip);
-  this.chart_area.selectAll(".task")
+  this.chart_area.append("g").attr("class", "y-axis").call(this.yAxis);
+
+  // Bars
+  this.chart_area
+    .selectAll(".bar")
     .data(this.processed_data)
     .enter()
     .append("rect")
-    .attr("class", "task")
-    .attr("y", function(d) {
-      return y_scale(d.task) - bar_width / 6;
-    })
-    .attr("x", function(d) {
-      return x_scale(d.start);
-    })
-    .attr("height", this.bar_width / 3)
-    .attr("width", function(d) {
-      return x_scale(d.end) - x_scale(d.start);
-    })
-    .on('mouseover', this.tip.show)
-    .on('mouseout', this.tip.hide);
-
-  return this;
-
+    .attr("class", "bar")
+    .attr("x", (d) => xScale(d.start))
+    .attr("y", (d) => yScale(d.task))
+    .attr("width", (d) => xScale(d.end) - xScale(d.start))
+    .attr("height", yScale.bandwidth());
 };
 
 function gantt(data, div, size) {
-
-  var inline_parser = function(data) {
-
-    var parse_date = d3.time.format("%d/%m/%Y").parse;
-
-    //Parse the dates
-    data.forEach(function(d) {
-      d.start = parse_date(d.start);
-      d.end = parse_date(d.end);
-    });
-
-
-    data.sort(function(a, b) {
-      return b.start - a.start;
+  var inline_parser = function (data) {
+    var parseDate = d3.timeParse("%Y-%m-%d");
+    data.forEach((d) => {
+      d.start = parseDate(d.start);
+      d.end = parseDate(d.end);
+      d.duration = (d.end - d.start) / (1000 * 60 * 60 * 24); // Convert to days
     });
 
     return data;
-
   };
 
-
-  var csv_parser = function(data) {
-
+  var csv_parser = function (data) {
     //To be written
-
   };
 
-  var draw = function(processed_data, div, size) {
-
-    var starts = processed_data.map(function(d) {
-      return d.start;
-    });
-
-    var ends = processed_data.map(function(d) {
-      return d.end;
-    });
-
-    var x_values = starts.concat(ends);
-
-    var y_values = processed_data.map(function(d) {
-      return d.task;
-    });
-
-    var scales = [create_scale(x_values, d3.time.scale()), create_scale(y_values, d3.scale.ordinal())];
-
-
+  var draw = function (processed_data, div, size) {
     var glasseye_chart = new Gantt(processed_data, div, size, scales);
-    glasseye_chart.add_svg().add_grid().add_tasks();
-
+    glasseye_chart.add_svg().add_tasks();
   };
 
   build_chart(data, div, size, undefined, csv_parser, inline_parser, draw);
-
 }
