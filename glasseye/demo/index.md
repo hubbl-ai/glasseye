5106f2987e9d0bd56ae709fcf40f48368f43d1b4
Ransome Madziyauswa <rmadziyauswa@gmail.com>
2025-03-18 14:57:03 +0200

    chord diagram

--- glasseye/demo/index.md
+++ glasseye/demo/index.md
@@ -437,3 +437,21 @@ data='
   colors='deep'
 >
 </reni>
+
+
+
+### A chord diagram 
+
+
+<chord
+data='[
+  [0, 5, 10, 2],
+  [5, 0, 3, 7],
+  [10, 3, 0, 6],
+  [2, 7, 6, 0]
+]
+'
+  size='{"width":500,"height":500}'
+  colors='deep'
+>
+</chord>
--- glasseye/ts/index.ts
+++ glasseye/ts/index.ts
@@ -1,5 +1,5 @@
 // Warning! THIS FILE WAS GENERATED! DO NOT EDIT!
-// Generated Tue Mar 18 14:44:20 CAT 2025
+// Generated Tue Mar 18 14:56:15 CAT 2025
 
 
 /// base.ts
@@ -1282,4 +1282,51 @@ export async function force(
 
     node.attr("cx", (d:any) => d.x!).attr("cy", (d:any) => d.y!);
   });
+}
+/// chord.ts
+
+export async function chord(
+  div: string = defaultArgumentObject.div,
+  data: any = defaultArgumentObject.data,
+  size: Size = defaultArgumentObject.size,
+  file?: DataFile,
+  colors: string[] = defaultArgumentObject.colors
+) {
+  const width = size.width, height = size.height, innerRadius = Math.min(width, height) * 0.4, outerRadius = innerRadius + 20;
+
+  const color = d3.scaleOrdinal(colors || d3.schemeCategory10);
+
+  const chord = d3.chord().padAngle(0.05).sortSubgroups(d3.descending);
+  const arc = d3.arc().innerRadius(innerRadius).outerRadius(outerRadius);
+  const ribbon = d3.ribbon().radius(innerRadius);
+
+  const chords = chord(data);
+
+  const svg = d3
+    .select(div)
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", `translate(${width / 2},${height / 2})`);
+
+  // Draw arcs
+  const group = svg.append("g")
+    .selectAll("g")
+    .data(chords.groups)
+    .enter().append("g");
+
+  group.append("path")
+    .attr("d", arc as any)
+    .style("fill", (_, i) => color(i.toString()))
+    .style("stroke", "#000");
+
+  // Draw ribbons
+  svg.append("g")
+    .selectAll("path")
+    .data(chords)
+    .enter().append("path")
+    .attr("d", ribbon as any)
+    .style("fill", d => color(d.source.index.toString()))
+    .style("stroke", "#000");
 }
