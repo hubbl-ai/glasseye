fc73d8b3d72029302a83202ad1cb331c5f82b248
Ransome Madziyauswa <rmadziyauswa@gmail.com>
2025-03-20 02:39:09 +0200

    chartmodule to glasseye module

--- glasseye/ts/rollup.config.js
+++ glasseye/ts/rollup.config.js
@@ -6,38 +6,7 @@ export default {
   output: {
     file: './dist/glasseyechart.js',
     format: 'iife', 
-    name: 'ChartModule',
+    name: 'Glasseye',
   },
   plugins: [resolve(), typescript()]
 };
-
-
-
-// import { defineConfig } from 'rollup';
-// import typescript from 'rollup-plugin-typescript2';
-
-// export default defineConfig({
-//   input: ['./linechart.ts', './GlasseyeChart.ts'], // List your input files
-//   output: {
-//     file: '../../../demo/js/bundle.js',
-//     format: 'iife', // Immediately Invoked Function Expression for browsers
-//     name: 'ChartModule', // Global variable for the bundle (used in browsers)
-//     sourcemap: true, // Optional: helpful for debugging
-//   },
-//   plugins: [typescript()],
-// });
-
-
-
-// import { defineConfig } from 'rollup';
-// import typescript from 'rollup-plugin-typescript2';
-
-// export default defineConfig({
-//   input: ['./linechart.ts', './GlasseyeChart.ts'],
-//   output: {
-//     dir: '../../../demo/js/', // Output directory
-//     format: 'es', // Use "esm" for better compatibility
-//     sourcemap: true,
-//   },
-//   plugins: [typescript()],
-// });
